e45240093f9c42441eec74cbaee01b9c99a0b5f6
Em Karisch <em@khaos.solutions>
2025-09-17 10:56:43 +0200

    feat: add red tint to active rail icon to make it distinguishable from normal rails

--- data-updates.lua
+++ data-updates.lua
@@ -3,3 +3,11 @@ if mods["SchallCircuitGroup"] then
   data.raw["rail-planner"]["active-rail"].subgroup = "circuit-input"
   data.raw.item["rail-combinator"].subgroup = "circuit-input"
 end
+
+-- Give the active rail a red tint to better distinguish it from normal rails.
+data.raw["rail-planner"]["active-rail"].icons = {
+  {
+    icon = data.raw["rail-planner"]["active-rail"].icon,
+    tint = {r = 1, g = 0, b = 0, a = 0.5}
+  },
+}
